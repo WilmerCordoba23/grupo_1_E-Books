cc23cf275594a17c4f3703a1276b4d1b2ee14918
WilmerCordoba23 <93687907+WilmerCordoba23@users.noreply.github.com>
2022-06-02 18:19:32 -0500

    Update app.js

--- Aplicación/app.js
+++ Aplicación/app.js
@@ -2,13 +2,12 @@ const express= require('express');
 const app= express();
 const path= require('path');
 
-app.listen(3030,()=>
-console.log("Servidor encendido")
-);
+app.listen(process.env.PORT || 3000,()=>
+console.log("Servidor encendido"));
 
 app.use(express.static('public'));
 
-app.get("/home",(req,res)=>
+app.get("/",(req,res)=>
 res.sendFile(path.join(__dirname,"./views/index.html"))
 );
 
