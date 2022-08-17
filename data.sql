41328b26dd1c4c89bc39a56e066879d70e2d2216
Sergioandres08 <sergioandrestorrez@gmail.com>
2022-08-16 21:51:15 -0500

    Base de Datos

---
+++ data.sql
@@ -0,0 +1,102 @@
+USE e_books;
+SET FOREIGN_KEY_CHECKS=0;
+
+insert into user ( first_name, last_name, email, password, image) values ( 'Nefen', 'Peplay', 'npeplay0@answers.com', 'ULdPKutPsfU', 'http://dummyimage.com/174x100.png/ff4444/ffffff');
+insert into user ( first_name, last_name, email, password, image) values ( 'Saxon', 'Josefson', 'sjosefson1@de.vu', 'SniRl6Qy', 'http://dummyimage.com/139x100.png/dddddd/000000');
+insert into user ( first_name, last_name, email, password, image) values ( 'Nanete', 'Tompion', 'ntompion2@abc.net.au', 'kbURDLb1ibG', 'http://dummyimage.com/105x100.png/cc0000/ffffff');
+insert into user ( first_name, last_name, email, password, image) values ( 'Charisse', 'Bettenay', 'cbettenay3@cpanel.net', 'HQSsIauB', 'http://dummyimage.com/231x100.png/dddddd/000000');
+insert into user ( first_name, last_name, email, password, image) values ( 'Felic', 'Probetts', 'fprobetts4@wunderground.com', 'UHDZqEj0Nmj0', 'http://dummyimage.com/154x100.png/ff4444/ffffff');
+insert into user ( first_name, last_name, email, password, image) values ( 'Kenny', 'Cracknall', 'kcracknall5@theatlantic.com', 'f0rDIS2D4sgO', 'http://dummyimage.com/228x100.png/ff4444/ffffff');
+insert into user ( first_name, last_name, email, password, image) values ( 'Elisha', 'Vida', 'evida6@imdb.com', 'Mlrwpzc9J', 'http://dummyimage.com/194x100.png/cc0000/ffffff');
+insert into user ( first_name, last_name, email, password, image) values ( 'Lil', 'Vittore', 'lvittore7@yandex.ru', '38WWVpm', 'http://dummyimage.com/149x100.png/dddddd/000000');
+insert into user ( first_name, last_name, email, password, image) values ( 'Obediah', 'Rushbrook', 'orushbrook8@pagesperso-orange.fr', 'D2cZDai', 'http://dummyimage.com/209x100.png/ff4444/ffffff');
+insert into user ( first_name, last_name, email, password, image) values ( 'Ellis', 'Eade', 'eeade9@bluehost.com', '9gAoca', 'http://dummyimage.com/105x100.png/ff4444/ffffff');
+
+
+
+
+
+
+insert into category ( name) values ('Casework');
+insert into category ( name) values ('Framing (Wood)');
+insert into category ( name) values ('Prefabricated Aluminum Metal Canopies');
+insert into category ( name) values ('Plumbing & Medical Gas');
+insert into category ( name) values ('Casework');
+insert into category ( name) values ('Drywall & Acoustical (FED)');
+insert into category ( name) values ('Structural & Misc Steel Erection');
+insert into category ( name) values ('Glass & Glazing');
+insert into category ( name) values ('Prefabricated Aluminum Metal Canopies');
+insert into category ( name) values ('Elevator');
+
+
+
+insert into genres ( name) values ('Drama|Musical|Romance');
+insert into genres ( name) values ('Drama|Thriller');
+insert into genres ( name) values ('Action|Drama|Thriller');
+insert into genres ( name) values ('Drama');
+insert into genres ( name) values ('Comedy|Drama');
+insert into genres ( name) values ('Drama|War');
+insert into genres ( name) values ('Comedy');
+insert into genres ( name) values ('Drama|Romance');
+insert into genres ( name) values ('Comedy');
+insert into genres ( name) values ('Drama');
+
+
+
+insert into format (name) values ('Digita');
+insert into format (name) values ('Tapa Dura');
+insert into format (name) values ('Tapa Blanda');
+
+
+
+
+insert into purchase (active) values (1);
+insert into purchase (active) values (0);
+insert into purchase (active) values (0);
+insert into purchase (active) values (1);
+insert into purchase (active) values (0);
+insert into purchase (active) values (1);
+insert into purchase (active) values (1);
+insert into purchase (active) values (0);
+insert into purchase (active) values (0);
+insert into purchase (active) values (1);
+
+insert into product (title, price, description, image, genre_id, category_id, purchase_id) values ('Night Train to Munich', 56.47, 'Sed sagittis. Nam congue, risus semper porta volutpat, quam pede lobortis ligula, sit amet eleifend pede libero quis orci. Nullam molestie nibh in lectus.
+
+Pellentesque at nulla. Suspendisse potenti. Cras in purus eu magna vulputate luctus.', 'http://dummyimage.com/148x100.png/cc0000/ffffff', 7, 5, 7);
+insert into product (title, price, description, image, genre_id, category_id, purchase_id) values ('Brother Bear', 21.05, 'Phasellus in felis. Donec semper sapien a libero. Nam dui.
+
+Proin leo odio, porttitor id, consequat in, consequat ut, nulla. Sed accumsan felis. Ut at dolor quis odio consequat varius.', 'http://dummyimage.com/100x100.png/ff4444/ffffff', 5, 10, 9);
+insert into product (title, price, description, image, genre_id, category_id, purchase_id) values ('Comin'' at Ya!', 50.46, 'Maecenas leo odio, condimentum id, luctus nec, molestie sed, justo. Pellentesque viverra pede ac diam. Cras pellentesque volutpat dui.
+
+Maecenas tristique, est et tempus semper, est quam pharetra magna, ac consequat metus sapien ut nunc. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris viverra diam vitae quam. Suspendisse potenti.', 'http://dummyimage.com/182x100.png/ff4444/ffffff', 1, 6, 6);
+insert into product (title, price, description, image, genre_id, category_id, purchase_id) values ('Nanny, The', 29.43, 'Maecenas leo odio, condimentum id, luctus nec, molestie sed, justo. Pellentesque viverra pede ac diam. Cras pellentesque volutpat dui.
+
+Maecenas tristique, est et tempus semper, est quam pharetra magna, ac consequat metus sapien ut nunc. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris viverra diam vitae quam. Suspendisse potenti.
+
+Nullam porttitor lacus at turpis. Donec posuere metus vitae ipsum. Aliquam non mauris.', 'http://dummyimage.com/147x100.png/5fa2dd/ffffff', 1, 3, 5);
+insert into product (title, price, description, image, genre_id, category_id, purchase_id) values ('St. Valentine''s Day Massacre, The', 10.27, 'Curabitur gravida nisi at nibh. In hac habitasse platea dictumst. Aliquam augue quam, sollicitudin vitae, consectetuer eget, rutrum at, lorem.', 'http://dummyimage.com/196x100.png/ff4444/ffffff', 6, 2, 8);
+insert into product (title, price, description, image, genre_id, category_id, purchase_id) values ('Werewolves on Wheels', 17.61, 'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla. Sed vel enim sit amet nunc viverra dapibus. Nulla suscipit ligula in lacus.', 'http://dummyimage.com/229x100.png/cc0000/ffffff', 4, 5, 2);
+insert into product (title, price, description, image, genre_id, category_id, purchase_id) values ('This Is My Father', 65.47, 'Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa. Donec dapibus. Duis at velit eu est congue elementum.
+
+In hac habitasse platea dictumst. Morbi vestibulum, velit id pretium iaculis, diam erat fermentum justo, nec condimentum neque sapien placerat ante. Nulla justo.', 'http://dummyimage.com/213x100.png/cc0000/ffffff', 8, 10, 1);
+insert into product (title, price, description, image, genre_id, category_id, purchase_id) values ('Atlantis: The Lost Empire', 1.12, 'Curabitur gravida nisi at nibh. In hac habitasse platea dictumst. Aliquam augue quam, sollicitudin vitae, consectetuer eget, rutrum at, lorem.', 'http://dummyimage.com/131x100.png/ff4444/ffffff', 1, 3, 5);
+insert into product (title, price, description, image, genre_id, category_id, purchase_id) values ('Trio', 56.0, 'Duis bibendum. Morbi non quam nec dui luctus rutrum. Nulla tellus.
+
+In sagittis dui vel nisl. Duis ac nibh. Fusce lacus purus, aliquet at, feugiat non, pretium quis, lectus.', 'http://dummyimage.com/189x100.png/dddddd/000000', 5, 7, 6);
+insert into product (title, price, description, image, genre_id, category_id, purchase_id) values ('Dead Men Walk', 54.67, 'Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vivamus vestibulum sagittis sapien. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+
+Etiam vel augue. Vestibulum rutrum rutrum neque. Aenean auctor gravida sem.', 'http://dummyimage.com/139x100.png/5fa2dd/ffffff', 10, 1, 4);
+
+insert into product_format (product_id, format_id) values (4, 2);
+insert into product_format (product_id, format_id) values (2, 1);
+insert into product_format (product_id, format_id) values (8, 2);
+insert into product_format (product_id, format_id) values (2, 3);
+insert into product_format (product_id, format_id) values (5, 1);
+insert into product_format (product_id, format_id) values (2, 3);
+insert into product_format (product_id, format_id) values (6, 3);
+insert into product_format (product_id, format_id) values (6, 1);
+insert into product_format (product_id, format_id) values (4, 3);
+insert into product_format (product_id, format_id) values (1, 3);
+
+
---
+++ structure.sql
@@ -0,0 +1,117 @@
+-- MySQL dump 10.13  Distrib 5.7.18, for Linux (x86_64)
+--
+-- Host: localhost    Database: laravel-database
+-- ------------------------------------------------------
+-- Server version	5.7.18-0ubuntu0.16.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+
+DROP DATABASE IF EXISTS e_books;
+CREATE DATABASE e_books;
+USE e_books;
+
+DROP TABLE IF EXISTS `product`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+
+CREATE TABLE `product` (
+     `id` int(10) unsigned  AUTO_INCREMENT NOT NULL,
+    `title` VARCHAR(100) NOT NULL,
+    `price` DECIMAL(10,2) unsigned NOT NULL,
+    `description` VARCHAR(1000) DEFAULT NULL,
+    `image` LONGBLOB  DEFAULT NULL,
+    `genre_id` int(10) unsigned NOT NULL,
+    `category_id` int(10) unsigned NOT NULL,
+    `purchase_id` int(10) unsigned NOT NULL,
+  PRIMARY KEY(`id`),
+  KEY `product_genre_id_foreign` (`genre_id`),
+  KEY `product_category_id_foreign` (`category_id`),
+  KEY `product_purchase_id_foreign` (`purchase_id`),
+  CONSTRAINT `product_genre_id_foreign` FOREIGN KEY (`genre_id`) REFERENCES `genres` (`id`),
+  CONSTRAINT `product_category_id_foreign` FOREIGN KEY (`category_id`) REFERENCES `category` (`id`),
+  CONSTRAINT `product_purchase_id_foreign` FOREIGN KEY (`purchase_id`) REFERENCES `purchase` (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+DROP TABLE IF EXISTS `genres`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `genres` (
+    `id` int(10) unsigned  AUTO_INCREMENT NOT NULL,
+    `name` VARCHAR(100)  NOT NULL,
+    PRIMARY KEY (`id`)
+)ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+DROP TABLE IF EXISTS `category`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `category` (
+    `id` int(10) unsigned  AUTO_INCREMENT NOT NULL,
+    `name` VARCHAR(100)  NOT NULL,
+     PRIMARY KEY (`id`)
+)ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+DROP TABLE IF EXISTS `purchase`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `purchase` (
+    `id` int(10) unsigned  AUTO_INCREMENT NOT NULL,
+    `active` tinyint(1) NOT NULL DEFAULT '1',
+     PRIMARY KEY (`id`)
+   
+)ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+DROP TABLE IF EXISTS `format`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `format` (
+    `id` int(10) unsigned  AUTO_INCREMENT NOT NULL,
+    `name` VARCHAR(100)  NOT NULL,
+     PRIMARY KEY (`id`)
+   
+)ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+DROP TABLE IF EXISTS `product_format`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `product_format` (
+     `id` int(10) unsigned  AUTO_INCREMENT NOT NULL,
+     `product_id` int(10) unsigned NOT NULL,
+     `format_id` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`id`),   
+  KEY `product_format_product_id_foreign` (`product_id`),
+  KEY `product_format_format_id_foreign` (`format_id`),
+  CONSTRAINT `product_format_product_id_foreign` FOREIGN KEY (`product_id`) REFERENCES `prduct` (`id`),
+  CONSTRAINT `product_format_format_id_foreign` FOREIGN KEY (`format_id`) REFERENCES `format` (`id`)
+)ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user` (
+    `id` int(10) unsigned  AUTO_INCREMENT NOT NULL,
+    `first_name` VARCHAR(100)  NOT NULL,
+    `last_name` VARCHAR(100)  NOT NULL,
+    `email` VARCHAR(100)  NOT NULL,
+    `password` VARCHAR(100)  NOT NULL,
+    `image` LONGBLOB  DEFAULT NULL,
+     PRIMARY KEY (`id`)
+   
+)ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
