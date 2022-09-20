984d667c4c929bb02547aadbf68fb1cb38ff688c
WilmerCordoba23 <wilwilmer058@gmail.com>
2022-09-20 14:21:38 -0500

    archivo para local o deploy

--- database/config/config.js
+++ database/config/config.js
@@ -1,3 +1,4 @@
+//deploy
 module.exports = {
   "development": {
     "username": "bfa00fb3ed90a8",
@@ -21,3 +22,29 @@ module.exports = {
     "dialect": "mysql"
   }
 }
+
+//local
+/* module.exports = {
+  "development": {
+    "username": "root",
+    "password": '',
+    "database": "e_books",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "test": {
+    "username": "root",
+    "password": null,
+    "database": "database_test",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "production": {
+    "username": "root",
+    "password": null,
+    "database": "database_production",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  }
+} */
+
