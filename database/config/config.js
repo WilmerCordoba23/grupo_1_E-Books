f89a7533839225a54b32cbd5d625cfbd0b8f3970
WilmerCordoba23 <wilwilmer058@gmail.com>
2022-09-19 20:10:14 -0500

    deploy

--- database/config/config.js
+++ database/config/config.js
@@ -7,17 +7,17 @@ module.exports = {
     "dialect": "mysql"
   }, 
   "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
+    "username": "bfa00fb3ed90a8",
+    "password": "e1b6ae11",
+    "database": "heroku_cbcdc8d2a5b85cf",
+    "host": "us-cdbr-east-06.cleardb.net",
     "dialect": "mysql"
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
+    "username": "bfa00fb3ed90a8",
+    "password": "e1b6ae11",
+    "database": "heroku_cbcdc8d2a5b85cf",
+    "host": "us-cdbr-east-06.cleardb.net",
     "dialect": "mysql"
   }
 }
