3377d09687a9151e37dbfa8bc3e5b7169f33b69f
Sergioandres08 <sergioandrestorrez@gmail.com>
2022-10-06 22:54:28 -0500

    Validaciones Front corregidas

--- public/Javascripts/formValidator.js
+++ public/Javascripts/formValidator.js
@@ -3,13 +3,28 @@ document.addEventListener('DOMContentLoaded', (event) => {
     // console.log('Hola, estoy validando');
 
     const UserForm = document.querySelector('#UserForm');
+    const email = document.getElementById('usuario');
+
 
     UserForm.addEventListener('submit', (event) => {
         const errorsArray = [];
         event.preventDefault();
+        let refuseEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+       
+        if(email == undefined){}
+        else{
+             let testEmail = refuseEmail.test(email.value)
+        
+             console.log(testEmail)
+        
+             if(email.value != '' /* || email.value != null || email.value != undefined */ && !testEmail){
+                    errorsArray.push(` El <strong> Email </strong> que intentas ingresar no es valido `);
+        }}
+        
+       
         Array.from(UserForm.elements).forEach(element => {
             
-            if(element.type !== 'submit'){
+            if(element.type !== 'submit' ){
                 console.log(element);
                 
                 // element.parentElement.removeChild(element.parentElement.lastElementChild);
@@ -23,8 +38,16 @@ document.addEventListener('DOMContentLoaded', (event) => {
                     element.classList.remove('is-invalid');
                 }
             }
+
+            if(element.value.length < 6 && element.value != "" && element.dataset.label != undefined && element.dataset.label != "Genero" && element.dataset.label != "Precio" && element.dataset.label != "Categoria")
             
+            {errorsArray.push(` El campo <strong>${element.dataset.label}</strong> debe contener al menos 6 caracteres `);}
+
+               
         });
+        
+ 
+
         if(errorsArray.length === 0){
             UserForm.submit();
         }else{
