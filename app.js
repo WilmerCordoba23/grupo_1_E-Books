04dcdf514853699e1bcfbb589c67c657e5763ab3
WilmerCordoba23 <wilwilmer058@gmail.com>
2022-10-18 13:02:18 -0500

    errors

--- app.js
+++ app.js
@@ -8,10 +8,10 @@ const path = require('path');
 const methodOverride =  require('method-override'); 
 const session = require('express-session');
 const cookieparser = require('cookie-parser');
-const morgan = require('morgan')
+const morgan = require('morgan');
 const cors = require('cors');
 
-app.use(cors());
+app.use(cors())
 
 app.use(morgan('dev'));
 
@@ -25,8 +25,8 @@ app.use(session({secret: "E-books"}));
 
 app.use(cookieparser());
 
-app.listen(process.env.PORT || 3000,()=>
-console.log("Servidor 3000 encendido"));
+app.listen(process.env.PORT || 4000,()=>
+console.log("Servidor 4000 encendido"));
 
 app.use(express.static('./public'));
 
@@ -43,5 +43,21 @@ app.use('/api',usuariosApi);
 //api productos
 app.use('/api',productosApi);
 
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    next(createError(404));
+  });
+  
+  // error handler
+  app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+  
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error', { msg: '500 - Ha ocurrido un error interno'});
+  });
 
 
