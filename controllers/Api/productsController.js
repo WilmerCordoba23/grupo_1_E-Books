4a357693d39c97d64bb0a1a581c14bf00f26939a
WilmerCordoba23 <wilwilmer058@gmail.com>
2022-10-18 15:54:12 -0500

    asincronico

--- controllers/Api/productsController.js
+++ controllers/Api/productsController.js
@@ -2,260 +2,268 @@ const db = require('../../database/models');
 const { producto } = require('../productsController');
 const sequelize = db.sequelize;
 
-        //Contador de Generos
-        let countFantasia=0
-        let countEconomia=0
-        let countLiteratura=0
-        let countAutoayuda=0
-        let countAccion=0
-        let countLiderazgo=0
-        let countFiccion=0
-        let countAventura=0
-        let countRomance=0
-        let countDrama=0
-        //Contador de Categorias
-        let masVendidos=0
-        let novedades=0
-        let recomendados=0
-        //Idenficador de Generos y Categorias
-        let genero
-        let categoria
+//Contador de Generos
+let countFantasia = 0
+let countEconomia = 0
+let countLiteratura = 0
+let countAutoayuda = 0
+let countAccion = 0
+let countLiderazgo = 0
+let countFiccion = 0
+let countAventura = 0
+let countRomance = 0
+let countDrama = 0
+//Contador de Categorias
+let masVendidos = 0
+let novedades = 0
+let recomendados = 0
+//Idenficador de Generos y Categorias
+let genero
+let categoria
 
 const productscontroller = {
-   
-   
-    products:(req,res)=>{
-       
-        db.product.findAll()     
-        .then(productDB => {
-
-        
-        let totalproduct = productDB.length;     
-        let products= productDB.map(product=>{
-            
-                if(product.genre_id==1){
-                    genero="Fantasia"
-                }
-                if(product.genre_id==2){
-                    genero="Economia"
-                }
-                if(product.genre_id==3){
-                    genero="Literatura"
-                }
-                if(product.genre_id==4){
-                    genero="Autoayuda"
-                }
-                if(product.genre_id==5){
-                    genero="Accion"
-                }
-                if(product.genre_id==6){
-                    genero="Liderazgo"
-                }
-                if(product.genre_id==7){
-                    genero="Ficcion"
-                }
-                if(product.genre_id==8){
-                    genero="Aventura"
-                }
-                if(product.genre_id==9){
-                    genero="Romance"
-                }
-                if(product.genre_id==10){
-                    genero="Drama"
-                }
 
 
+    products: async (req, res) => {
 
-                if(product.category_id==1){
-                    categoria="Mas Vendidos"
-                }
-                if(product.category_id==2){
-                    categoria="Novedades"
-                }
-                if(product.category_id==3){
-                    categoria="Recomendados"
-                }
-        
+        try {
+            await db.product.findAll()
+                .then(productDB => {
 
 
-            return{
-                id:product.id,
-                name:product.title,
-                description:product.description,
-                categoria:{
-                    'name':categoria,
-                    'id':product.category_id},
-                genero:{
-                    'name':genero,
-                    'id':product.genre_id
-                },
-                detail:`https://grupo-1-e-books.herokuapp.com/api/products/${product.id}`
-            }
-        });
-            for(let i=0; i<productDB.length; i++){
-                if(productDB[i].genre_id==1){
-                    countFantasia++
-                }
-                if(productDB[i].genre_id==2){
-                    countEconomia++
-                }
-                if(productDB[i].genre_id==3){
-                    countLiteratura++
-                }
-                if(productDB[i].genre_id==4){
-                    countAutoayuda++
-                }
-                if(productDB[i].genre_id==5){
-                    countAccion++
-                }
-                if(productDB[i].genre_id==6){
-                    countLiderazgo++
-                }
-                if(productDB[i].genre_id==7){
-                    countFiccion++
-                }
-                if(productDB[i].genre_id==8){
-                    countAventura++
-                }
-                if(productDB[i].genre_id==9){
-                    countRomance++
-                }
-                if(productDB[i].genre_id==10){
-                    countDrama++
-                }
-            }
+                    let totalproduct = productDB.length;
+                    let products = productDB.map(product => {
 
-            for(let i=0; i<productDB.length; i++){
-                if(productDB[i].category_id==1){
-                    masVendidos++
-                }
-                if(productDB[i].category_id==2){
-                    novedades++
-                }
-                if(productDB[i].category_id==3){
-                    recomendados++
-                }
-            }
-        
-        let totalcategory={
-            'Mas vendidos':masVendidos,
-            'Novedades':novedades,
-            'Recomendados':recomendados
+                        if (product.genre_id == 1) {
+                            genero = "Fantasia"
+                        }
+                        if (product.genre_id == 2) {
+                            genero = "Economia"
+                        }
+                        if (product.genre_id == 3) {
+                            genero = "Literatura"
+                        }
+                        if (product.genre_id == 4) {
+                            genero = "Autoayuda"
+                        }
+                        if (product.genre_id == 5) {
+                            genero = "Accion"
+                        }
+                        if (product.genre_id == 6) {
+                            genero = "Liderazgo"
+                        }
+                        if (product.genre_id == 7) {
+                            genero = "Ficcion"
+                        }
+                        if (product.genre_id == 8) {
+                            genero = "Aventura"
+                        }
+                        if (product.genre_id == 9) {
+                            genero = "Romance"
+                        }
+                        if (product.genre_id == 10) {
+                            genero = "Drama"
+                        }
+
+
+
+                        if (product.category_id == 1) {
+                            categoria = "Mas Vendidos"
+                        }
+                        if (product.category_id == 2) {
+                            categoria = "Novedades"
+                        }
+                        if (product.category_id == 3) {
+                            categoria = "Recomendados"
+                        }
+
+
+
+                        return {
+                            id: product.id,
+                            name: product.title,
+                            description: product.description,
+                            categoria: {
+                                'name': categoria,
+                                'id': product.category_id
+                            },
+                            genero: {
+                                'name': genero,
+                                'id': product.genre_id
+                            },
+                            detail: `https://grupo-1-e-books.herokuapp.com/api/products/${product.id}`
+                        }
+                    });
+                    for (let i = 0; i < productDB.length; i++) {
+                        if (productDB[i].genre_id == 1) {
+                            countFantasia++
+                        }
+                        if (productDB[i].genre_id == 2) {
+                            countEconomia++
+                        }
+                        if (productDB[i].genre_id == 3) {
+                            countLiteratura++
+                        }
+                        if (productDB[i].genre_id == 4) {
+                            countAutoayuda++
+                        }
+                        if (productDB[i].genre_id == 5) {
+                            countAccion++
+                        }
+                        if (productDB[i].genre_id == 6) {
+                            countLiderazgo++
+                        }
+                        if (productDB[i].genre_id == 7) {
+                            countFiccion++
+                        }
+                        if (productDB[i].genre_id == 8) {
+                            countAventura++
+                        }
+                        if (productDB[i].genre_id == 9) {
+                            countRomance++
+                        }
+                        if (productDB[i].genre_id == 10) {
+                            countDrama++
+                        }
+                    }
+
+                    for (let i = 0; i < productDB.length; i++) {
+                        if (productDB[i].category_id == 1) {
+                            masVendidos++
+                        }
+                        if (productDB[i].category_id == 2) {
+                            novedades++
+                        }
+                        if (productDB[i].category_id == 3) {
+                            recomendados++
+                        }
+                    }
+
+                    let totalcategory = {
+                        'Mas vendidos': masVendidos,
+                        'Novedades': novedades,
+                        'Recomendados': recomendados
+                    }
+
+
+                    let totalgender = {
+                        "Fantasia": countFantasia,
+                        "Economia": countEconomia,
+                        "Literatura": countLiteratura,
+                        "Autoayuda": countAutoayuda,
+                        "Accion": countAccion,
+                        "Liderazgo": countLiderazgo,
+                        "Ficcion": countFiccion,
+                        "Aventura": countAventura,
+                        "Romance": countRomance,
+                        "Drama": countDrama
+                    }
+
+
+
+
+                    if (productDB) {
+                        //console.log(productDB)
+
+                        res.status(200).json({
+                            "count": totalproduct,
+                            "countByGender": totalgender,
+                            "countByCategory": totalcategory,
+                            "products": products,
+                            "status": 200,
+                            "msg": "ok",
+                            "endpoint": "/api/products"
+                        })
+                    }
+                })
+        } catch (error) {
+            console.log(error);
+            res.status(500).json({ 'msg': '500 - Ha ocurrido un error interno' });
+
         }
 
-            
-            let totalgender ={
-                "Fantasia":countFantasia,
-                "Economia":countEconomia,
-                "Literatura":countLiteratura,
-                "Autoayuda":countAutoayuda,
-                "Accion":countAccion,
-                "Liderazgo":countLiderazgo,
-                "Ficcion":countFiccion,
-                "Aventura":countAventura,
-                "Romance":countRomance,
-                "Drama":countDrama
-            }
+    },
+    productsdetail: async (req, res) => {
+        try {
+            await db.product.findOne({ where: { id: req.params.id } })
+                .then(productDB => {
+                    if (productDB.genre_id == 1) {
+                        genero = "Fantasia"
+                    }
+                    if (productDB.genre_id == 2) {
+                        genero = "Economia"
+                    }
+                    if (productDB.genre_id == 3) {
+                        genero = "Literatura"
+                    }
+                    if (productDB.genre_id == 4) {
+                        genero = "Autoayuda"
+                    }
+                    if (productDB.genre_id == 5) {
+                        genero = "Accion"
+                    }
+                    if (productDB.genre_id == 6) {
+                        genero = "Liderazgo"
+                    }
+                    if (productDB.genre_id == 7) {
+                        genero = "Ficcion"
+                    }
+                    if (productDB.genre_id == 8) {
+                        genero = "Aventura"
+                    }
+                    if (productDB.genre_id == 9) {
+                        genero = "Romance"
+                    }
+                    if (productDB.genre_id == 10) {
+                        genero = "Drama"
+                    }
 
 
 
+                    if (productDB.category_id == 1) {
+                        categoria = "Mas Vendidos"
+                    }
+                    if (productDB.category_id == 2) {
+                        categoria = "Novedades"
+                    }
+                    if (productDB.category_id == 3) {
+                        categoria = "Recomendados"
+                    }
 
-        if(productDB){
-            //console.log(productDB)
 
-            res.status(200).json({
-                "count":totalproduct,
-                "countByGender":totalgender,
-                "countByCategory":totalcategory,
-                "products":products,
-                "status":200,
-                "msg":"ok",
-                "endpoint":"/api/products"
-            })
+                    if (productDB) {
+                        res.status(200).json({
+                            id: productDB.id,
+                            title: productDB.title,
+                            description: productDB.description,
+                            price: productDB.price,
+                            categoria: {
+                                'name': categoria,
+                                'id': productDB.category_id
+                            },
+                            genero: {
+                                'name': genero,
+                                'id': productDB.genre_id
+                            },
+                            image: `https://grupo-1-e-books.herokuapp.com/images/products/${productDB.image}`,
+                            "status": 200,
+                            "msg": "ok",
+                            "endpoint": `/api/products/${productDB.id}`
+                        })
+                    }
+                })
+                .catch(errors => {
+                    res.status(404).json({
+                        "status": 404,
+                        "msg": "error",
+                    })
+                })
+        } catch (error) {
+            console.log(error);
+            res.status(500).json({ 'msg': '500 - Ha ocurrido un error interno' });
+
         }
-        })                
-        /*.catch(errors => {
-            res.status(404).json({
-                "status":404,
-                "msg":"error",
-            })
-             })*/
-             
-        },
-        productsdetail:(req,res)=>{
-        db.product.findOne({where:{id:req.params.id}})
-        .then(productDB => {
-            if(productDB.genre_id==1){
-                genero="Fantasia"
-            }
-            if(productDB.genre_id==2){
-                genero="Economia"
-            }
-            if(productDB.genre_id==3){
-                genero="Literatura"
-            }
-            if(productDB.genre_id==4){
-                genero="Autoayuda"
-            }
-            if(productDB.genre_id==5){
-                genero="Accion"
-            }
-            if(productDB.genre_id==6){
-                genero="Liderazgo"
-            }
-            if(productDB.genre_id==7){
-                genero="Ficcion"
-            }
-            if(productDB.genre_id==8){
-                genero="Aventura"
-            }
-            if(productDB.genre_id==9){
-                genero="Romance"
-            }
-            if(productDB.genre_id==10){
-                genero="Drama"
-            }
-
-
-
-            if(productDB.category_id==1){
-                categoria="Mas Vendidos"
-            }
-            if(productDB.category_id==2){
-                categoria="Novedades"
-            }
-            if(productDB.category_id==3){
-                categoria="Recomendados"
-            }
-    
-        
-        if(productDB){
-            res.status(200).json({
-                id:productDB.id,
-                title:productDB.title,
-                description:productDB.description,
-                price:productDB.price,
-                categoria:{
-                    'name':categoria,
-                    'id':productDB.category_id},
-                genero:{
-                    'name':genero,
-                    'id':productDB.genre_id
-                },
-                image:`https://grupo-1-e-books.herokuapp.com/images/products/${productDB.image}`,
-                "status":200,
-                "msg":"ok",
-                "endpoint":`/api/products/${productDB.id}`
-            })
-        }
-        })                
-        .catch(errors => {
-            res.status(404).json({
-                "status":404,
-                "msg":"error",
-            })
-             })
-        }
+    }
 }
 
 module.exports = productscontroller;
--- controllers/Api/usersController.js
+++ controllers/Api/usersController.js
@@ -3,66 +3,76 @@ const { usuario } = require('../usersController');
 const sequelize = db.sequelize;
 
 const userscontroller = {
-   
-   
-    users:(req,res)=>{
-       
-     db.user.findAll()
-     .then(userDB => {
-        let totaluser = userDB.length;
-        let users= userDB.map(user=>{
-            return{
-                id:user.id,
-                first_name:user.first_name,
-                last_name:user.last_name,
-                email:user.email,
-                detail:`https://grupo-1-e-books.herokuapp.com/api/user/${user.id}`
+
+
+    users: async (req, res) => {
+
+        try {
+
+            const userDB = await db.user.findAll();
+
+
+            if (userDB) {
+                const totaluser = userDB.length;
+                const users = userDB.map(user => {
+                    return {
+                        id: user.id,
+                        first_name: user.first_name,
+                        last_name: user.last_name,
+                        email: user.email,
+                        detail: `https://grupo-1-e-books.herokuapp.com/api/user/${user.id}`
+                    }
+                })
+
+                res.status(200).json({
+                    "count": totaluser,
+                    "users": users,
+                    "status": 200,
+                    "msg": "ok",
+                    "endpoint": "/api/users"
+                });
+            } else {
+                res.status(404).json({ 'msg': 'No hay datos para mostrar' });
             }
-        });
 
-        if(userDB){
-            res.status(200).json({
-                "count":totaluser,
-                "users":users,
-                "status":200,
-                "msg":"ok",
-                "endpoint":"/api/users"
-            })
-        }
-        })                
-        .catch(errors => {
-            res.status(404).json({
-                "status":404,
-                "msg":"error",
-            })
-             })
+        } catch (error) {
+            console.log(error);
+            res.status(500).json({ 'msg': '500 - Ha ocurrido un error interno' });
 
-        },
-        usersdetail:(req,res)=>{
-            db.user.findOne({where:{id:req.params.id}})
-     .then(userDB => {
+        }
 
-        
-        if(userDB){
-            res.status(200).json({
-                id:userDB.id,
-                first_name:userDB.first_name,
-                last_name:userDB.last_name,
-                email:userDB.email,
-                image:`https://grupo-1-e-books.herokuapp.com/images/users/${userDB.image}`,
-                "status":200,
-                "msg":"ok",
-                "endpoint":`/api/users/${userDB.id}`
-            })
+    },
+    usersdetail: async (req, res) => {
+
+        try {
+
+            const userDB = await db.user.findOne({ where: { id: req.params.id } });
+
+
+            if (userDB) {
+
+
+                res.status(200).json({
+                    id: userDB.id,
+                        first_name: userDB.first_name,
+                        last_name: userDB.last_name,
+                        email: userDB.email,
+                        image: `https://grupo-1-e-books.herokuapp.com/images/users/${userDB.image}`,
+                        "status": 200,
+                        "msg": "ok",
+                        "endpoint": `/api/users/${userDB.id}`
+                });
+            } else {
+                res.status(404).json({ 'msg': 'No hay datos para mostrar' });
+            }
+
+        } catch (error) {
+            console.log(error);
+            res.status(500).json({ 'msg': '500 - Ha ocurrido un error interno' });
+
         }
-        })                
-        .catch(errors => {
-            res.status(404).json({
-                "status":404,
-                "msg":"error",
-            })
-             })
-        }
+
+    }
 }
 
 module.exports = userscontroller;
